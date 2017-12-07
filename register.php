a268e6c65b6425782a6877c7acfbc9069aa73ac1
Ardee <ardee@github.com>
2017-12-07 21:14:58 +0530

    format

--- register.php
+++ register.php
@@ -6,32 +6,36 @@
 <body>
   <div class = "container">
     <header>
+      <p align=left><a href="login.php">Back to Homepage</a></p>
       <h1>Register</h1>
     </header>
       <form method="post"  action="register.php">
         <div class="input-group">
-          <label>First Name </label>
+          <label>First Name : </label>
           <input type="text" name="firstname">
         </div>
         <div class="input-group">
-          <label>Surame </label>
+          <label>Surame : </label>
           <input type="text" name="surname">
         </div>
         <div class="input-group">
-          <label>Email</label>
+          <label>Email :</label>
           <input type="text" name="email">
         </div>
         <div class="input-group">
-          <label>Password</label>
+          <label>Password :</label>
           <input type="password" name="password_1">
         </div>
         <div class="input-group">
-          <label>Confirm Password</label>
+          <label>Confirm Password :</label>
           <input type="password" name="password_2">
         </div>
         <div class="input-group">
             <button type="submit" name="register" class="btn">Register</button>
         </div>
+        <p>
+          Already a member? <a href="login.php">Sign in</a>
+      </form>
     <footer><center>copyright information</center>
     </footer>
   </div>
