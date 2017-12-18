3e6517ae37bfca69fe085fc9ad9b6584ca346aa0
Ardee <ardee@github.com>
2017-12-18 19:50:50 +0530

    bootstrap edits

--- homepage.php
+++ homepage.php
@@ -1,46 +1,83 @@
 <?php include('server.php'); ?>
-<html>
+<html lang="en">
 <head>
-<style>
-div.container {
-    width: 100%;
-    border: 5px solid gray;
-}
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-header, footer {
-    padding: 1em;
-    color: white;
-    background-color: black;
-    clear: left;
-    text-align: left;
-}
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+  </script>
+  <style>
+  .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
 
-nav {
-    float: right;
-    max-width: 200px;
-    margin: 0;
-    padding: 1em;
-}
-
-
-article {
-    margin-right: 190px;
-    border-right: 1px solid gray;
-    padding: 1em;
-    overflow: hidden;
-}
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+      text-align: center;
+    }
+    .jumbotron {
+        margin-bottom: 0;
+      }
 </style>
+
 <title>Athena : Login</title>
 </head>
 <body>
 
-<div class="container">
+  <div class="jumbotron">
+    <div class="container text-center">
+      <h1>Athena</h1>
+      <p>Bookshelf IITR Online Portal</p>
+    </div>
+  </div>
+  <div class="container-fluid">
+   <div class="modal-body row">
 
-<header>
-   <h1>Athena</h1>
-</header>
+     <div class="col-sm-10">
 
-<nav>
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+         <ol class="carousel-indicators">
+           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+           <li data-target="#myCarousel" data-slide-to="1"></li>
+           <li data-target="#myCarousel" data-slide-to="2"></li>
+         </ol>
+
+
+         <div class="carousel-inner">
+           <div class="item active">
+             <img src="screen_1.png" alt="Los Angeles">
+           </div>
+
+           <div class="item">
+             <img src="screen_1.png" alt="Chicago">
+           </div>
+
+           <div class="item">
+             <img src="screen_1.png" alt="New York">
+           </div>
+         </div>
+
+         <!-- Left and right controls -->
+         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+           <span class="glyphicon glyphicon-chevron-left"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="right carousel-control" href="#myCarousel" data-slide="next">
+           <span class="glyphicon glyphicon-chevron-right"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div>
+       </div>
+
+     <div class="col-sm-2 sidenav">
+
   <h2> Login</h2>
   <form method="post" action="homepage.php">
 
@@ -48,24 +85,24 @@ article {
 
     <label>Email ID</label>
       <input type ="text" name="email">
+
     <br><br>
+
       <label>Password</label>
       <input type ="password" name="password">
+
     <br><br>
       <button type="submit" name="login">Login</button>
   </form>
   Not yet a member? <a href="register-form.php">Register here</a>
-</nav>
-
-<article>
-  <h1>Welcome to Athena</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor nec sapien ut suscipit. Praesent porttitor risus dolor. Nullam dignissim magna dui, vel posuere libero bibendum ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vel magna tristique porttitor eget eget diam. Proin nec ullamcorper lorem, id ullamcorper mauris. Pellentesque tempor orci id luctus pellentesque. Proin placerat fermentum elit, id mattis nulla fringilla in. Sed ut turpis felis.</p>
-  <p>Duis gravida ligula eu cursus blandit. Praesent interdum odio erat, ac pretium dui finibus eget. Etiam quis tortor ligula. Morbi suscipit dui est, eu suscipit lorem convallis nec. Aliquam molestie mauris luctus, consequat turpis sed, vehicula tellus. Aliquam dignissim neque tortor, in tempor metus volutpat et. Vestibulum ac libero non velit tempor interdum. Sed nec mi sit amet odio blandit ultricies non a metus. Nulla eleifend porta purus. Nullam aliquet orci ac dolor elementum porttitor. Aliquam risus massa, malesuada id urna sed, hendrerit commodo mauris. Donec commodo gravida leo quis condimentum. Duis tempor dictum leo, vitae faucibus augue feugiat ac. Nulla vel justo maximus, condimentum quam id, luctus quam.</p>
-</article>
-
-<footer><center>copyright information</center></footer>
+</div>
 
 </div>
+<footer class="container-fluid">
+  <p>Copyright information</p>
+</footer>
+
+
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,19 +3,42 @@
 
 
 ?>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-    <title>Athena</title>
-  </head>
-  <body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+  </script>
+  <style>
 
-    <div class="header">
-      <header>
-      <h1 align="left">Catalogue </h1>
-      </header>
-    </div>
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+
+  .carousel-inner img {
+      width: 100%;
+      margin: auto;
+      min-height:200px;
+  }
+
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none;
+    }
+  }
+  </style>
+</head>
+<body>
 
     <div class="container">
       <?php if (isset($_SESSION['success'])): ?>
@@ -32,6 +55,82 @@
 
     <?php endif ?>
   </div>
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Catalogue</a></li>
+        <li><a href="#">Borrow</a></li>
+        <li><a href="#">Share</a></li>
+        <li><a href="#">My Profile</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image">
+        <div class="carousel-caption">
+          <h3>New book added</h3>
+          <p>Submit borrow request now!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image">
+        <div class="carousel-caption">
+          <h3>New feauture added!</h3>
+          <p>(Yay)</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="container text-center">
+  <h3>Catalogue</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Book 1</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Book 2</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Book 3</p>
+    </div>
+
     <div class="footer">
       <footer><center>copyright information</center></footer>
 
